25ff594bf5d303a56186bb523770212f6c0eb66f
mehulgohil-tal <mehul.gohil@talentica.com>
2025-09-01 23:07:08 +0530

    Implement GitHub Pages SPA redirect and enhance routing compatibility

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,12 +7,23 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
+// Hash-based routing for GitHub Pages compatibility
+function hashLocation() {
+  return window.location.hash.replace(/^#/, "") || "/";
+}
+
+function hashNavigate(to: string) {
+  window.location.hash = to;
+}
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter hook={() => [hashLocation(), hashNavigate]}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
